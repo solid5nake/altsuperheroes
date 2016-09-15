bd566020abd136090508f8f6622cb03d3694d2ed
solid5nake <engelenburg1980@gmail.com>
2016-09-15 14:16:14 +0200

    Added export default marvel to marvel-api.js

--- src/marvel-api.js
+++ src/marvel-api.js
@@ -4,5 +4,6 @@ const api = require('marvel-api');
 
 const marvel = api.createClient({
   publicKey: '0da4ad81cc85813a45808444fab73ce8',
-  privateKey: '0a66903344aed265871d714dbaac207d332d824b'
 });
+
+export default marvel;
